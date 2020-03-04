c55b14d92173f25141dc5db99f818bb94961e03d
aimiolek7 <60266334+aimiolek7@users.noreply.github.com>
2020-03-04 13:22:51 -0500

    I added the code to plot theta, but I think I messed up somewhere.

--- Step 4/plot_acceleration_time.py
+++ Step 4/plot_acceleration_time.py
@@ -1,5 +1,6 @@
 import matplotlib.pyplot as plt
 import numpy as np
+import math
 
 import os
 path="C:/Users/abbyi/Documents/Freshman/ES2/GitHub/project-1-abigail-and-alex/Step 4"
@@ -15,6 +16,22 @@ def acc_time (file):
     plt.ylabel('Acceleration of Pendulum')
     plt.title('Acceleration vs Time of Pendulum' + " at Length " + file[7])
     plt.show()
+    
+    
+def angle_time (file):
+    len_1= np.loadtxt(file,  delimiter=",")
+    acc_y=len_1[0:,1]
+    acc_z=len_1[0:,3]
+    t=len_1[0:,0]
+    a=np.exp(acc_y)
+    b=np.exp(acc_z)
+    c=np.add(a,b)
+    theta=np.sqrt(c)
+    plt.plot(t/1000,theta)
+    plt.xlabel('Time')
+    plt.ylabel('Angle of Pendulum')
+    plt.title('Angle vs Time of Pendulum' + " at Length " + file[7])
+    plt.show()
 
 fin =open("Length 1.txt",encoding='utf8')
 fin2 =open("Length 2.txt",encoding='utf8')
@@ -22,11 +39,8 @@ fin3 =open("Length 3.txt",encoding='utf8')
 fin4 =open("Length 4.txt",encoding='utf8')
 fin5 =open("Length 5.txt",encoding='utf8')
 
-acc_time ("Length 1.txt") 
-acc_time ("Length 2.txt") 
-acc_time ("Length 3.txt") 
-acc_time ("Length 4.txt") 
-acc_time ("Length 5.txt") 
+angle_time ("Length 2.txt") 
+
 
 
 plt.show()
