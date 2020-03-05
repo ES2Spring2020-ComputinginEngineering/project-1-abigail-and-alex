264ebd27081de2d2df7d6ccd863184570cfd6ac5
aimiolek7 <60266334+aimiolek7@users.noreply.github.com>
2020-03-05 11:43:26 -0500

    Finished Step 4

--- Step 2/step1_theoretical_period.py
+++ Step 2/step1_theoretical_period.py
@@ -3,7 +3,7 @@ import matplotlib.pyplot as plt
 
 
 
-lengths=np.array([5,9,13,17.25, 21.25])
+lengths=np.array([5,9,13,17.25, 21.25])*0.0254
 
 def estimated_period (length):
         length=np.divide (length,9.81)
@@ -16,8 +16,10 @@ period = estimated_period(lengths)
 plt.plot(lengths,period)
 plt.xlabel('Length of Pendulum')
 plt.ylabel('Period of Pendulum')
-plt.title('lenght vs Period of a Pendulum')
+plt.title('length vs Period of a Pendulum')
 plt.show()
+
+print(period)
         
 #This assumes no air resistance or frictions
 # or any outside factors effect the period of
--- Step 4/plot_acceleration_time.py
+++ Step 4/plot_acceleration_time.py
@@ -12,9 +12,11 @@ def acc_time (file):
     len_1= np.loadtxt(file,  delimiter=",")
     t=len_1[0:,0]
     x=len_1[0:,1]
-    plt.plot(t,x)
-    plt.xlabel('Time')
-    plt.ylabel('Acceleration of Pendulum')
+    y=len_1[0:,2]
+    plt.plot(t/1000,x)
+    plt.plot(t/1000,y)
+    plt.xlabel('Time (in seconds)')
+    plt.ylabel('Acceleration of Pendulum (in milli-g)')
     plt.title('Acceleration vs Time of Pendulum' + " at Length " + file[7])
     plt.show()
     
@@ -25,6 +27,7 @@ def angle_time (file):
     acc_y=len_1[0:,2]
     acc_z=len_1[0:,3]
     t=len_1[0:,0]
+    x= acc_x**2
     y = acc_y**2
     z = acc_z**2
     bottom_half = y+z
@@ -32,9 +35,15 @@ def angle_time (file):
     theta = acc_x/bottom_half
     theta = np.arctan(theta)
     
-    plt.plot(t,theta)
-    plt.xlabel('Time')
-    plt.ylabel('Angle of Pendulum')
+    bottom_half2 = x+z
+    bottom_half2 = np.sqrt(bottom_half2)
+    theta2 = acc_y/bottom_half2
+    theta2 = np.arctan(theta2)
+    
+    plt.plot(t/1000,theta)
+    plt.plot(t/1000, theta2)
+    plt.xlabel('Time (in seconds)')
+    plt.ylabel('Angle of Pendulum (in radians)')
     plt.title('Angle vs Time of Pendulum' + " at Length " + file[7])
     plt.show()
 
@@ -59,25 +68,48 @@ def Period_of_Wave(file):
                old_max = t[i]
             new_max = t[i]
             counter +=1
-            print("old max: " , old_max)
-            print("new max: ", new_max)
-            print("diff: ", new_max-old_max)
             time_between_periods += (new_max - old_max)
             old_max = t[i]
     time_between_periods = time_between_periods / (max_iterations+1)
-    return time_between_periods
+    return time_between_periods/1000
     
-
+def period_length(file1, file2, file3, file4, file5):
+    p1=Period_of_Wave(file1)
+    p2=Period_of_Wave(file2)
+    p3=Period_of_Wave(file3)
+    p4=Period_of_Wave(file4)
+    p5=Period_of_Wave(file5)
+    period=np.array([p1,p2,p3,p4,p5])
+    print(period)
+    lengths=np.array([5,9,13,17.25, 21.25])*0.0254
+    
+    
+   
+    plt.scatter(lengths, period)
+    plt.xlabel('Lengths (in meters)')
+    plt.ylabel('Time between Period (in seconds)')
+    plt.title('Time between Periods vs Length')
+    plt.show()
+    
+    
 fin =open("Length 1.txt",encoding='utf8')
 fin2 =open("Length 2.txt",encoding='utf8')
 fin3 =open("Length 3.txt",encoding='utf8')
 fin4 =open("Length 4.txt",encoding='utf8')
 fin5 =open("Length 5.txt",encoding='utf8')
+fin52 =open("Length 5.2.txt",encoding='utf8')
 
 angle_time ("Length 1.txt") 
 acc_time("Length 1.txt")
-x = Period_of_Wave("Length 1.txt")
-print("the Time Between Periods is: ", x)
+angle_time ("Length 2.txt") 
+acc_time("Length 2.txt")
+angle_time ("Length 3.txt") 
+acc_time("Length 3.txt")
+angle_time ("Length 4.txt") 
+acc_time("Length 4.txt")
+angle_time ("Length 5.2.txt") 
+acc_time("Length 5.2.txt")
+period_length("Length 1.txt", "Length 2.txt", "Length 3.txt", "Length 4.txt", "Length 5.2.txt")
 
 
 plt.show()
@@ -86,3 +118,4 @@ fin2.close()
 fin3.close()
 fin4.close()
 fin5.close()
+fin52.close()
