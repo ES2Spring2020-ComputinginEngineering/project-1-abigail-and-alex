9b52e89035dc51fd04e171d71e79614805a90ba6
aimiolek7 <60266334+aimiolek7@users.noreply.github.com>
2020-03-24 15:23:41 -0500

    Attempted to fix the problems with the graphs

--- Step 5/Pendulum Simulation.py
+++ Step 5/Pendulum Simulation.py
@@ -8,6 +8,8 @@ import numpy as np
 import matplotlib.pyplot as plt
 import math 
 
+# GLOBAL VARIABLES
+lengths=np.array([5,9,13,17.25, 21.25])*0.0254
 
 
 def update_system(acc,pos,vel,time1,time2,length):
@@ -25,26 +27,29 @@ def print_system(time,pos,vel,acc):
     print("ACCELERATION ", acc, "\n")
 
 # initial conditions
-l = 2
-theta = 3.14/4
+l = 5*0.0254
+theta = math.pi/2
 pos = [theta]
 vel = [0]
-acc = [0,0]
-acc[1] = -9.8*math.sin(theta)/l
-time = np.linspace(0,20,201)
+acc = [0,-9.8*math.sin(theta)/l]
+time = np.linspace(0,10,201)
 
 
 print_system(time[0],pos[0],vel[0],acc[0])
 i = 1
+t=[0]
 while i < len(time):
     # update position and velocity using previous values and time step
-    posNext, velNext, accNext = update_system(acc[i],pos[i-1],vel[i-1],time[i-1],time[i],l)
+    posNext, velNext, accNext = update_system(acc[i-1],pos[i-1],vel[i-1],time[i-1],time[i],l)
     pos.append(posNext)
     vel.append(velNext)
     acc.append(accNext)
+    t.append(i)
     #print_system(time[i],pos[i],vel[i],acc[i])
     i += 1
 
 
-plt.plot(time,vel)
+plt.plot(t,vel)
+plt.plot(t,pos)
+plt.plot(t,acc)
 plt.show()
