5f928485ffc792b5352ff638da233ebc90651e21
AlexBazyk <60264151+AlexBazyk@users.noreply.github.com>
2020-03-28 20:56:25 -0400

    period more or less done
    error with multiplying maybe

--- Step 5/Pendulum Simulation.py
+++ Step 5/Pendulum Simulation.py
@@ -28,8 +28,41 @@ def print_system(time,pos,vel,acc):
     print("VELOCITY: ", vel)
     print("ACCELERATION ", acc, "\n")
 
+def wave_period(pos):
+    peaks = sig.find_peaks(pos)
+    peaks = tuple(peaks[0])
+    old_max = 0
+    new_max = 0
+    counter = 0
+    per = []
+    diff = 0
+    
+    for i in range(len(pos)):
+        if(i == peaks[counter]):
+            if(counter == len(peaks)-1):
+                break
+            if(counter == 0):
+                old_max = i
+            new_max = i
+            print("peak ", new_max)
+            counter +=1
+            diff = new_max - old_max
+            per.append(diff)
+            old_max = i
+
+    sum = 0
+    for i in range(len(per)):
+        print("diff in peaks, ", per[i])
+        sum += per[i]
+    #You have to convert this time into seconds. I think this is the way to do it.
+    sum = sum/(len(peaks)-1)
+    sum /= len(pos)
+    
+    return sum
+        
+
 # initial conditions
-l = .5#5*0.0254
+l = .5
 theta = math.pi/2
 pos = [theta]
 vel = [0]
@@ -57,26 +90,4 @@ plt.show()
 plt.plot(t,acc, "blue")
 plt.show()
 
-
-peaks = sig.find_peaks(pos)
-peaks = tuple(peaks[0])
-old_max = 0
-new_max = 0
-counter = 0
-time_of_periods = 0
-
-for i in range(len(pos)):
-    if(i == peaks[counter]):
-        if(counter == len(peaks)-1):
-            break
-        if(counter == 0):
-            old_max = i
-        new_max = i
-        counter +=1
-        time_of_periods += (new_max - old_max)
-        print(time_of_periods)
-        old_max = i
-time_of_periods = time_of_periods / (len(peaks)-1)
-time_of_periods /= len(pos)
-
-print(time_of_periods)
+print("period ", wave_period(pos))
